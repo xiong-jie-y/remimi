f2ab172cc5bf0443a630c5c123af27cd29a6fea0
xiong-jie-y <fwvillage@gmail.com>
2021-04-05 21:06:58 +0900

    Add depth models (wrapper)

--- remimi/sensors/paseudo_camera.py
+++ remimi/sensors/paseudo_camera.py
@@ -1,5 +1,9 @@
 import os
 import glob
+from remimi.monodepth.ken3d.depthestim import Ken3DDepthEstimator
+from remimi.monodepth.dpt import DPTDepthEstimator
+# from remimi.monodepth.adabin import InferenceHelper
+from remimi.segmentation.rgb_segmentation import SemanticSegmenter
 import torch
 import cv2
 import argparse
@@ -14,147 +18,63 @@ from dpt.transforms import Resize, NormalizeImage, PrepareForNet
 
 from util.misc import visualize_attention
 
-def get_inverse_map(depth, bits=2):
-    depth_min = depth.min()
-    depth_max = depth.max()
-
-    # max_val = (2 ** (8 * bits)) - 1
-
-    depth_max = 3000
-
-    # if depth_max - depth_min > np.finfo("float").eps:
-    out = depth_max - depth
-    # out = max_val - depth
-    
-    return out
-
 import enum
 class ImageType(enum.Enum):
     RGB = "rgb"
     BGR = "bgr"
 
+import matplotlib.pyplot as plt
+
+def show_image_blocking(image, **imshow_arg):
+    fig = plt.figure(frameon=False)
+    # fig.set_size_inches(image.shape[1], image.shape[0])
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(image, aspect='auto', **imshow_arg)
+    plt.show()
+
 class DPTPaseudoDepthCamera:
-    def __init__(self, model_path, sensor, model_type="dpt_hybrid", optimize=True, debug=False, output_type=ImageType.RGB, boundary_depth_removal=False):
-        print("initialize")
-
-        # set torch options
-        torch.backends.cudnn.enabled = True
-        torch.backends.cudnn.benchmark = True
-
-        # select device
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print("device: %s" % device)
-
-        net_w = net_h = 384
-
-        # load network
-        if model_type == "dpt_large":  # DPT-Large
-            model = DPTDepthModel(
-                path=model_path,
-                backbone="vitl16_384",
-                non_negative=True,
-                enable_attention_hooks=debug,
-            )
-            normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        elif model_type == "dpt_hybrid":  # DPT-Hybrid
-            model = DPTDepthModel(
-                path=model_path,
-                backbone="vitb_rn50_384",
-                non_negative=True,
-                enable_attention_hooks=debug,
-            )
-            normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        elif model_type == "midas_v21":  # Convolutional model
-            model = MidasNet_large(model_path, non_negative=True)
-            normalization = NormalizeImage(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            )
-        else:
-            assert (
-                False
-            ), f"model_type '{model_type}' not implemented, use: --model_type [dpt_large|dpt_hybrid|midas_v21]"
-
-        transform = Compose(
-            [
-                Resize(
-                    net_w,
-                    net_h,
-                    resize_target=None,
-                    keep_aspect_ratio=True,
-                    ensure_multiple_of=32,
-                    resize_method="minimal",
-                    image_interpolation_method=cv2.INTER_CUBIC,
-                ),
-                normalization,
-                PrepareForNet(),
-            ]
-        )
-
-        model.eval()
-
-        if optimize == True and device == torch.device("cuda"):
-            model = model.to(memory_format=torch.channels_last)
-            model = model.half()
-
-        model.to(device)
-
-        self.model = model
-        self.transform = transform
-        self.device = device
-        self.optimize = optimize
-
-        # self.cap = cv2.VideoCapture(camera_id)
-        self.debug = debug
+    def __init__(self, sensor, model_name: str, debug=False, output_type=ImageType.RGB, boundary_depth_removal=False):
+        if model_name == "dpt":
+            self.depth_estimator = DPTDepthEstimator(debug)
+        elif model_name == "ken3d":
+            self.depth_estimator = Ken3DDepthEstimator()
+        # self.depth_estimator = InferenceHelper()
 
         self.sensor = sensor
         self.output_type = output_type
         self.boundary_depth_removal = boundary_depth_removal
+        self.semantic_segmentater = SemanticSegmenter()
 
     def get_color_and_depth(self):
-        img = self.sensor.get_color()
+        color = self.sensor.get_color()
+        # color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # img = util.io.read_image(img_name)
-        img_input = self.transform({"image": img})["image"]
+        depth = self.depth_estimator.estimate_depth(color)
 
-        # compute
-        with torch.no_grad():
-            sample = torch.from_numpy(img_input).to(self.device).unsqueeze(0)
-
-            if self.optimize == True and self.device == torch.device("cuda"):
-                sample = sample.to(memory_format=torch.channels_last)
-                sample = sample.half()
-
-            prediction = self.model.forward(sample)
-            prediction = (
-                torch.nn.functional.interpolate(
-                    prediction.unsqueeze(1),
-                    size=img.shape[:2],
-                    mode="bicubic",
-                    align_corners=False,
-                )
-                .squeeze()
-                .cpu()
-                .numpy()
-            )
-
-            if self.debug:
-                visualize_attention(sample, self.model, prediction, args.model_type)
-
-        color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        depth = get_inverse_map(prediction.astype(np.uint16))
+        # import IPython; IPython.embed()
 
         if self.boundary_depth_removal:
-            color2 = np.copy(color)
-            cv2.cvtColor(color2, cv2.COLOR_RGB2GRAY)
+            color2 = self.semantic_segmentater.convert_to_semantic_image(color)
+            color_rgb = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
+            cv2.imshow("semantic", color2)
+            # color2 = np.copy(smimg)
+            # show_image_blocking(color_rgb)
+            color2 = cv2.cvtColor(color2, cv2.COLOR_RGB2GRAY)
+            color2[color2 > 0] = 255
+            # show_image_blocking(color2, cmap = "gray")
             canny_img = cv2.Canny(color2, 50, 110)
             kernel = np.ones((5,5),np.uint8)
-            canny_img = cv2.dilate(canny_img,kernel,iterations = 2)
+            canny_img = cv2.dilate(canny_img,kernel,iterations = 1)
 
             cv2.imshow("test2222", canny_img)
 
             depth[canny_img == 255] = 0
 
+        color = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
         if self.output_type == ImageType.BGR:
-            return img, depth
+            return color, depth
         elif self.output_type == ImageType.RGB:
             return color, depth
         else:
