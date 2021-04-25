bc57e833b3b563f886c13dbed5f469442da5a0be
xiong-jie-y <fwvillage@gmail.com>
2021-04-25 17:55:51 +0900

    Instance mask generation improvement.

--- examples/data_conversion/create_mask_image_dataset.py
+++ examples/data_conversion/create_mask_image_dataset.py
@@ -1,28 +1,49 @@
+from os.path import join
+from remimi.sensors.file import MultipleImageStream
 from remimi.sensors.edit_stream import SaveMaskAndFrameSink
 from remimi.sensors.webcamera import SimpleWebcamera
 import click
 import cv2
+import glob
 
 from remimi.sensors import StreamFinished
 
 @click.command()
-@click.option("--input-file")
+@click.option("--video-file")
+@click.option("--image-folder")
 @click.option("--output-folder")
 @click.option("--class-names", multiple=True)
 @click.option("--margin", type=int, default=1)
-def main(input_file, output_folder, class_names, margin):
-    video_stream = SimpleWebcamera(input_file)
+def main(video_file, image_folder, output_folder, class_names, margin):
+    if video_file:
+        video_stream = SimpleWebcamera(video_file)
+    elif image_folder:
+        aa = sorted(list(glob.glob(join(image_folder, "*.jpg"))) + list(glob.glob(join(image_folder, "*.png"))))
+        video_stream = MultipleImageStream(aa)
+    else:
+        raise RuntimeError("No input.")
     sink = SaveMaskAndFrameSink(video_stream, output_folder, class_names, margin)
 
+    frame_no = 0
+    wait_time = 1
+    if image_folder is not None:
+        wait_time = 4000
     while True:
+        # if frame_no < 1000:
+        #     frame_no += 1
+        #     video_stream.get_color()
+        #     continue
+
         try:
             sink.process(show=True)
         except StreamFinished:
             break
 
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(wait_time)
         if key == ord('a'):
             break
+
+        frame_no += 1
 
 
 if __name__ == "__main__":
--- remimi/detection/instance_segmentation.py
+++ remimi/detection/instance_segmentation.py
@@ -1,18 +1,30 @@
 from typing import List
 from pkg_resources import resource_filename
-from remimi.utils.file import get_model_file
+from remimi.utils.file import get_model_file, get_model_file_from_gdrive
 import numpy as np
 
 from mmdet.apis import inference_detector, init_detector
 
+import cv2
+
 class InstanceSegmenter:
     def __init__(self):
         device = 'cuda:0'
-        config_file = resource_filename("remimi", "detection/configs/yolact/yolact_r50_1x8_coco.py")
+        # config_file = resource_filename("remimi", "detection/configs/yolact/yolact_r50_1x8_coco.py")
+        # checkpoint_file = get_model_file(
+        #     "yolact_r50_1x8_coco_20200908-f38d58df.pth",
+        #     "https://openmmlab.oss-cn-hangzhou.aliyuncs.com/mmdetection/v2.0/yolact/yolact_r50_1x8_coco_20200908-f38d58df.pth"
+        # )
+        config_file = resource_filename("remimi", "detection/configs/point_rend/point_rend_r50_caffe_fpn_mstrain_3x_coco.py")
         checkpoint_file = get_model_file(
-            "yolact_r50_1x8_coco_20200908-f38d58df.pth",
-            "https://openmmlab.oss-cn-hangzhou.aliyuncs.com/mmdetection/v2.0/yolact/yolact_r50_1x8_coco_20200908-f38d58df.pth"
+            "point_rend_r50_caffe_fpn_mstrain_1x_coco-1bcb5fb4.pth",
+            "http://download.openmmlab.com/mmdetection/v2.0/point_rend/point_rend_r50_caffe_fpn_mstrain_1x_coco/point_rend_r50_caffe_fpn_mstrain_1x_coco-1bcb5fb4.pth"
         )
+        # config_file = resource_filename("remimi", "detection/configs/detectors/detectors_htc_r50_1x_coco.py")
+        # checkpoint_file = get_model_file(
+        #     "detectors_htc_r50_1x_coco-329b1453.pth",
+        #     "http://download.openmmlab.com/mmdetection/v2.0/detectors/detectors_htc_r50_1x_coco/detectors_htc_r50_1x_coco-329b1453.pth"
+        # )
         self.model = init_detector(config_file, checkpoint_file, device=device)
         self.previous_seg = None
 
@@ -41,7 +53,7 @@ class InstanceSegmenter:
                     self.previous_seg = seg
                     not_found = False
 
-        if not_found:
-            mask[self.previous_seg == 1] = 0
+        # if not_found:
+        #     mask[self.previous_seg == 1] = 0
 
         return mask
